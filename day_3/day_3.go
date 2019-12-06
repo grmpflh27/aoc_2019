d568c7838649210148f3b3c968797135eeaff56d
Thomas Beyer <thomas.beyer@hotmail.de>
2019-12-05 22:59:17 -0800

    aoc day 3 - pretty long and verbose solution using only native golang constructs for some 2d grid intersection algorithm with manhattan distance

--- day_3/day_3.go
+++ day_3/day_3.go
@@ -12,45 +12,61 @@ type Coord struct {
 	Y int
 }
 
-func parseMoves(strMoves []string) []Coord {
-	moves := make([]Coord, len(strMoves))
+type Move struct {
+	from      Coord
+	to        Coord
+	direction string
+}
 
-	for i, cur := range strMoves {
-		dir := string(cur[0])
-		num, _ := strconv.Atoi(cur[1:])
-		x, y := 0, 0
-		switch dir {
-		case "L":
-			x = -num
-		case "R":
-			x = num
-		case "U":
-			y = num
-		case "D":
-			y = -num
+func (m Move) distance() int {
+	return Abs(m.to.Y-m.from.Y) + Abs(m.to.X-m.from.X)
+}
+
+func parseMoves(strMoves []string) []Move {
+	moves := make([]Move, len(strMoves))
+
+	curCoord := Coord{0, 0}
+	for i := range strMoves {
+		nextCoord, direction := parseCoord(strMoves[i], curCoord)
+		moves[i] = Move{
+			curCoord,
+			nextCoord,
+			direction,
 		}
-		moves[i] = Coord{x, y}
+		curCoord = nextCoord
+
 	}
 	return moves
 }
 
-func walk(moves []Coord) []Coord {
-	visitedCoords := make([]Coord, len(moves)+1)
-	start := Coord{0, 0}
-
-	visitedCoords[0] = start
-
-	for i, _ := range moves {
-		visitedCoords[i+1] = Coord{visitedCoords[i].X + moves[i].X, visitedCoords[i].Y + moves[i].Y}
+func parseCoord(strMove string, coord Coord) (Coord, string) {
+	dir := string(strMove[0])
+	num, _ := strconv.Atoi(strMove[1:])
+	x, y := 0, 0
+	switch dir {
+	case "L":
+		x = coord.X - num
+		y = coord.Y
+	case "R":
+		x = coord.X + num
+		y = coord.Y
+	case "U":
+		y = coord.Y + num
+		x = coord.X
+	case "D":
+		y = coord.Y - num
+		x = coord.X
 	}
-	return visitedCoords
+	return Coord{x, y}, dir
 }
 
-func initBoard(stitched []Coord) [][]int {
-	// create rightly sized board
+type Board [][]int
 
+func initBoard(stitched []Move) (Board, Coord) {
+	// create rightly sized board
 	minX, maxX, minY, maxY := 0, 0, 0, 0
-	for _, coord := range stitched {
+	for _, move := range stitched {
+		coord := move.to
 		if minX > coord.X {
 			minX = coord.X
 		}
@@ -65,16 +81,169 @@ func initBoard(stitched []Coord) [][]int {
 		}
 	}
 
+	originOffset := Coord{-1 * minX, -1 * minY}
+	fmt.Printf("Origin offset: %v\n", originOffset)
+
 	sizeX := maxX - minX + 1
 	sizeY := maxY - minY + 1
 	fmt.Printf("Building board %v x %v\n", sizeX, sizeY)
 
-	board := make([][]int, sizeY)
+	board := make(Board, sizeY)
 	for i := range board {
 		board[i] = make([]int, sizeX)
 	}
 
-	return board
+	return board, originOffset
+}
+
+func transposeMoves(moves []Move, originOffset Coord) {
+	idx, end := 0, len(moves)
+	for idx < end {
+		m := &moves[idx]
+		m.from.X = m.from.X + originOffset.X
+		m.from.Y = m.from.Y + originOffset.Y
+		m.to.X = m.to.X + originOffset.X
+		m.to.Y = m.to.Y + originOffset.Y
+		idx++
+	}
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		x *= -1
+	}
+	return x
+}
+
+func placeFootsteps(moves []Move, board Board) {
+
+	for _, move := range moves {
+		switch move.direction {
+		case "L", "R":
+			start := Min(move.from.X, move.to.X)
+			end := Max(move.from.X, move.to.X)
+			for i := start; i <= end; i++ {
+				if board[move.from.Y][i] != 1 {
+					board[move.from.Y][i] = 1
+				}
+			}
+		case "U", "D":
+			start := Min(move.from.Y, move.to.Y)
+			end := Max(move.from.Y, move.to.Y)
+			for i := start; i <= end; i++ {
+				if board[i][move.from.X] != 1 {
+					board[i][move.from.X] = 1
+				}
+			}
+		}
+	}
+}
+
+func findIntersections(board1 Board, board2 Board, originOffset Coord) []Coord {
+	intersections := []Coord{}
+	for rowIdx := range board1 {
+		for colIdx := range board1[rowIdx] {
+			if board1[rowIdx][colIdx] == 1 && board2[rowIdx][colIdx] == 1 {
+				cur := Coord{colIdx, rowIdx}
+				if cur != originOffset {
+					intersections = append(intersections, cur)
+				}
+			}
+		}
+	}
+	return intersections
+}
+
+func manhattanDistance(from Coord, to Coord) int {
+	return Abs(to.X-from.X) + Abs(to.Y-from.Y)
+}
+
+func minManhattanDistance(intersections []Coord, origin Coord) (int, Coord) {
+	manh := 100000
+	minCoord := origin
+
+	for _, inter := range intersections {
+		if inter != origin {
+			curDistance := manhattanDistance(origin, inter)
+			if manh > curDistance {
+				manh = curDistance
+				minCoord = inter
+			}
+		}
+	}
+	return manh, minCoord
+}
+
+func countStepsToMinIntersection(moves []Move, intersections []Coord) []int {
+	stepCounts := make([]int, len(intersections))
+
+	for i, inter := range intersections {
+		stepCounts[i] = countStepsTo(moves, inter)
+	}
+	return stepCounts
+}
+
+func countStepsTo(moves []Move, intersection Coord) int {
+	var steps int = 0
+	for _, move := range moves {
+		switch move.direction {
+		case "R":
+			start := Min(move.from.X, move.to.X)
+			end := Max(move.from.X, move.to.X)
+			for i := start + 1; i <= end; i++ {
+				if move.to.Y == intersection.Y && i == intersection.X {
+					steps++
+					return steps
+				}
+				steps++
+			}
+		case "L":
+			start := Min(move.from.X, move.to.X) - 1
+			end := Max(move.from.X, move.to.X) - 1
+			for i := end; i > start; i-- {
+				if move.to.Y == intersection.Y && i == intersection.X {
+					steps++
+					return steps
+				}
+				steps++
+			}
+		case "U":
+			start := Min(move.from.Y, move.to.Y)
+			end := Max(move.from.Y, move.to.Y)
+			for i := start + 1; i <= end; i++ {
+				if i == intersection.Y && move.to.X == intersection.X {
+					steps++
+					return steps
+				}
+				steps++
+			}
+		case "D":
+			start := Min(move.from.Y, move.to.Y) - 1
+			end := Max(move.from.Y, move.to.Y) - 1
+			for i := end; i > start; i-- {
+				if i == intersection.Y && move.to.X == intersection.X {
+					steps++
+					return steps
+				}
+				steps++
+			}
+		}
+	}
+	return steps
 }
 
 func main() {
@@ -84,22 +253,38 @@ func main() {
 	fmt.Println("==========")
 	words := aoc2019_shared.LoadStr(day)
 	//1
-	coords1 := walk(parseMoves(words[0]))
-	fmt.Printf("FIRST GUY WALKS %v\n", coords1)
+	moves1 := parseMoves(words[0])
+	moves2 := parseMoves(words[1])
 
-	coords2 := walk(parseMoves(words[1]))
-	fmt.Printf("SECOND GUY WALKS: %v\n", coords2, coords2)
+	stitched := append(moves1, moves2...)
+	board1, _ := initBoard(stitched)
+	board2, originOffset := initBoard(stitched)
 
-	stitched := append(coords1, coords2...)
-	board := initBoard(stitched)
+	transposeMoves(moves1, originOffset)
+	transposeMoves(moves2, originOffset)
 
-	// 1st walks
-	// for i, move := range coords1 {
+	placeFootsteps(moves1, board1)
+	placeFootsteps(moves2, board2)
 
-	// }
+	intersections := findIntersections(board1, board2, originOffset)
+	minDistance, _ := minManhattanDistance(intersections, originOffset)
+	fmt.Printf("Answer 1: %v\n", minDistance)
 
-	fmt.Printf("Answer 1: %v\n", board)
 	// 2
-	fmt.Printf("Answer 2: %v\n")
+	cnt1 := countStepsToMinIntersection(moves1, intersections)
+	cnt2 := countStepsToMinIntersection(moves2, intersections)
+
+	sums := make([]int, len(cnt1))
+	for i := range cnt1 {
+		sums[i] = cnt1[i] + cnt2[i]
+	}
+
+	minSum := sums[0]
+	for _, curSum := range sums[1:] {
+		if curSum < minSum {
+			minSum = curSum
+		}
+	}
+	fmt.Printf("Answer 2: %v\n", minSum)
 
 }
